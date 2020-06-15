39a1ae9d2fb2085345cb07a1b20bbf1abe0f1d6a
Lloyd Hill <Lloyd.Hill@myClearwater.com>
2020-06-15 17:34:29 -0400

    Initial commit.  Removed all redundant setting from app20 originial project

---
+++ Index.aspx
@@ -0,0 +1,173 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Index.aspx.vb" Inherits="FeeEstimimator.GovDeptsPlanningIndex" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        CityOfClearwater/FeeEstimator
+        <p>
+            &nbsp;
+        </p>
+        <p>
+            &nbsp;
+        </p>
+        <table width="100%" cellspacing="0" cellpadding="0">
+            <td align="center" width="50%">
+                <table width="55%" cellspacing="0" cellpadding="0">
+
+                    <tr>
+                        <td align="center" colspan="3">
+                            <h3 style="text-align: center;">
+                                <asp:Label ID="lblMessage" runat="server" CssClass="Alert" Font-Bold="True" Font-Size="Small" ForeColor="Red"></asp:Label>&nbsp;</h3>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td align="left">Type Of Permit</td>
+                        <td colspan="2" align="left">
+                            <asp:DropDownList ID="ddlPermitType" runat="server"></asp:DropDownList>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td align="left">Type Of Property:</td>
+                        <td align="left">
+                            <asp:RadioButton ID="Residential" runat="server" GroupName="ConstructionType" />
+                            Residential
+                        </td>
+                        <td align="left">
+                            <asp:RadioButton ID="Commercial" runat="server" GroupName="ConstructionType" />
+                            Commercial
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td align="left">Value Of Job:</td>
+                        <td align="left" colspan="2">
+                            <asp:TextBox ID="txtValue" runat="server" Width="15em" Height="1.2em"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td align="left">Trades Included:</td>
+                        <td align="left" colspan="2">
+                            <asp:CheckBox ID="ckbBuilding" Text="Building" runat="server" />
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <td align="left"></td>
+                        <td align="left" colspan="2">
+                            <asp:CheckBox ID="ckbElectric" Text="Electric" runat="server" />
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <td align="left"></td>
+                        <td align="left" colspan="2">
+                            <asp:CheckBox ID="ckbRoof" Text="Roof" runat="server" /></td>
+                    </tr>
+
+
+                    <tr>
+                        <td align="left"></td>
+                        <td align="left" colspan="2">
+                            <asp:CheckBox ID="ckbMechanic" Text="Mechanic" runat="server" /></td>
+                    </tr>
+
+                    <tr>
+                        <td align="left"></td>
+                        <td align="left" colspan="2">
+                            <asp:CheckBox ID="ckbPlumbing" Text="Plumbing" runat="server" /></td>
+                    </tr>
+
+
+                    <tr>
+                        <td align="left"></td>
+                        <td align="left" colspan="2">
+                            <asp:CheckBox ID="ckbGas" Text="Gas" runat="server" /></td>
+                    </tr>
+
+                    <tr>
+                        <td>&nbsp;</td>
+                    </tr>
+                </table>
+
+                <table style="margin: 0 auto; width: 100%;">
+                    <tr>
+                        <td style="height: 10px; text-align: center; width: 100%">
+                            <asp:Panel ID="pnlResults" runat="server" BackColor="LightGray" Width="200" Height="165" BorderColor="black" BorderWidth="2">
+                                <br />
+                                <asp:Label ID="lblEstimate" CssClass="Alert" runat="server" Font-Bold="True" Font-Size="Small" ForeColor="Red"></asp:Label>
+                                <hr />
+                                <asp:Label ID="lblPlansFee" runat="server" Font-Bold="True" Font-Size="Small" ForeColor="black"></asp:Label>
+                                <br />
+                                <asp:Label ID="lblPermitFee" runat="server" Font-Bold="True" Font-Size="Small" ForeColor="black"></asp:Label>
+                                <br />
+                                <asp:Label ID="lblTrades" runat="server" Font-Bold="True" Font-Size="Small" ForeColor="black"></asp:Label>
+                                <br />
+                                <asp:Label ID="lblStateFee" runat="server" Font-Bold="True" Font-Size="Small" ForeColor="black"></asp:Label>
+                                <hr />
+                                <asp:Label ID="lblTotal" runat="server" Font-Bold="True" Font-Size="Small" ForeColor="black"></asp:Label>
+                                <br />
+                                <br />
+                            </asp:Panel>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td colspan="1" style="height: 10px; text-align: center;">
+                            <asp:Button ID="txtSubmit" runat="server" Text="Submit" Height="1.7em" Width="6em" />
+                            <asp:Button ID="txtReset" runat="server" Text="Reset" Height="1.7em" Width="6em" />
+                        </td>
+                    </tr>
+                </table>
+                <br />
+                <br />
+                <table width="75%" cellspacing="0" cellpadding="0">
+
+                    <tr>
+                        <td align="center">Click <a href="http://www.myclearwater.com/home/showdocument?id=2261">here </a>to view the schedule of fees, rates and charges.
+	               
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>&nbsp;</td>
+                    </tr>
+
+
+                    <tr>
+                        <td align="left">
+                            <asp:Label ID="lblExplainState" CssClass="Alert" runat="server" Font-Bold="True" Font-Size="Small" ForeColor="black"></asp:Label>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <asp:Label ID="lblDisclaimer" CssClass="Alert" runat="server" Font-Bold="True" Font-Size="Small" ForeColor="Red"></asp:Label>
+                        </td>
+                    </tr>
+                </table>
+
+            </td>
+        </table>
+
+    </form>
+</body>
+</html>
---
+++ Index.aspx.vb
@@ -0,0 +1,201 @@
+﻿
+Imports System.Data.SqlClient
+
+Partial Class GovDeptsPlanningIndex
+    Inherits System.Web.UI.Page
+    Private cnStr As ConnectionStringSettingsCollection
+    Private AppSettings As NameValueCollection
+    Dim TradeNumbers As Integer
+
+    Public Sub New()
+        cnStr = System.Configuration.ConfigurationManager.ConnectionStrings
+        AppSettings = System.Configuration.ConfigurationManager.AppSettings
+    End Sub
+
+    Protected Sub Page_Init(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Init
+        Dim cmdCommon As New SqlCommand
+        Dim drCommon As SqlDataReader = Nothing
+
+        pnlResults.Visible = False
+        Commercial.Checked = True
+
+        lblExplainState.Text = "*   State Fee includes <i>Fl Building Commission </i> <u>and</u> <i> Fl Building Code Administration and Inspectors Board</i>. They are 3% of the Permit Fees, Plans Fee and Trades Fee or a minimum of $2."
+        lblDisclaimer.Text = "Disclaimer:   The above fee estimate does not include other fees such as impact fees, fire fees, right-of-way, tree removal and clearing and grubbing fees, some of which could be substantial.  For information concerning fire fees please call 727-562-4327 and for more information on other fees please call 727-562-4567. The City of Clearwater assumes no liability for any errors, omissions, or inaccuracies in the information provided. "
+        Try
+            With cmdCommon
+                .Connection = New SqlConnection(cnStr.Item("cnFeeEstimator").ConnectionString)
+                .Connection.Open()
+                .CommandType = CommandType.StoredProcedure
+                .CommandText = "spGetFeeTypes"
+                drCommon = .ExecuteReader
+            End With
+
+            With ddlPermitType
+                .DataSource = drCommon
+                .DataValueField = "CASE_TYPE"
+                .DataTextField = "PERMIT_TYPE"
+                .DataBind()
+            End With
+        Catch err As Exception
+            lblMessage.Text = "Error retrieving domains - Contact IT Help Desk<br>" &
+             "Details: " & err.Message
+        Finally
+            If Not IsNothing(drCommon) Then
+                If Not drCommon.IsClosed Then drCommon.Close()
+                drCommon = Nothing
+            End If
+            If Not IsNothing(cmdCommon.Connection) Then
+                If Not cmdCommon.Connection.State = ConnectionState.Closed Then cmdCommon.Connection.Close()
+            End If
+            cmdCommon = Nothing
+        End Try
+
+
+    End Sub
+
+    Protected Sub txtSubmit_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtSubmit.Click
+
+        Session("PermitType") = ddlPermitType.SelectedIndex.ToString()
+        lblMessage.Text = Session("PermitType")
+
+
+        lblMessage.Text = ""
+        pnlResults.Visible = False
+
+        Dim ResCom As String
+        Dim TradeNumbers As Integer = 0
+        Dim OverTheCounter As Integer = 0
+
+
+        If Not (IsNumeric(Trim(txtValue.Text))) Then
+            lblMessage.Text = "Please enter a numeric job value"
+            Exit Sub
+        End If
+
+        If ddlPermitType.SelectedIndex = 0 Then
+            lblMessage.Text = "Please enter a valid permit type"
+            Exit Sub
+        End If
+
+        If Trim(txtValue.Text) = "" Then
+            lblMessage.Text = "Please enter a valid permit value"
+            Exit Sub
+        End If
+
+        If Not (Commercial.Checked) And Not (Residential.Checked) Then
+            lblMessage.Text = "Please select commercial or residential property"
+            Exit Sub
+        End If
+
+        Session("PermitValue") = Double.Parse(Replace(Trim(txtValue.Text), "$", ""))
+
+        If Commercial.Checked Then ResCom = "Commercial"
+        If Residential.Checked Then ResCom = "Residential"
+
+        If ckbBuilding.Checked Then
+            TradeNumbers = TradeNumbers + 1
+        End If
+
+        If ckbElectric.Checked Then
+            TradeNumbers = TradeNumbers + 1
+        End If
+
+        If ckbRoof.Checked Then
+            TradeNumbers = TradeNumbers + 1
+        End If
+
+        If ckbMechanic.Checked Then
+            TradeNumbers = TradeNumbers + 1
+        End If
+
+        If ckbPlumbing.Checked Then
+            TradeNumbers = TradeNumbers + 1
+        End If
+
+        If ckbGas.Checked Then
+            TradeNumbers = TradeNumbers + 1
+        End If
+
+        If ddlPermitType.SelectedValue.Substring(0, 16) <> "Over the Counter" Then
+            OverTheCounter = 0
+        Else
+            OverTheCounter = 1
+        End If
+
+        Dim cmdCalculate As New SqlCommand
+        Dim drCalculate As SqlDataReader
+
+        Try
+            With cmdCalculate
+                .Connection = New SqlConnection(cnStr.Item("cnFeeEstimator").ConnectionString)
+                .Connection.Open()
+                .CommandType = CommandType.StoredProcedure
+                .CommandText = "spCalculateFee"
+                .Parameters.Add("@ResCom", SqlDbType.VarChar).Value = ResCom
+                .Parameters.Add("@Value", SqlDbType.Int).Value = Session("PermitValue")
+                .Parameters.Add("@Trades", SqlDbType.Int).Value = TradeNumbers
+                .Parameters.Add("@OverTheCounter", SqlDbType.Int).Value = OverTheCounter
+                drCalculate = .ExecuteReader(CommandBehavior.CloseConnection)
+            End With
+
+            If drCalculate.Read Then
+                Session("PermitFee") = drCalculate.Item("PermitFee")
+                Session("PlansFee") = drCalculate.Item("PlansFee")
+                Session("Trades") = drCalculate.Item("Trades")
+                Session("StateFee") = drCalculate.Item("StateFee")
+                Session("Total") = drCalculate.Item("Total")
+
+
+                If OverTheCounter = 0 Then
+                    lblPlansFee.Text = "Review Fee: " & FormatCurrency(Session("PlansFee"))
+                Else
+                    lblPlansFee.Text = ""
+                End If
+                pnlResults.Visible = True
+                lblEstimate.Text = "Estimated Fees"
+                lblPermitFee.Text = "Permit Fee: " & FormatCurrency(Session("PermitFee"))
+                lblStateFee.Text = "State Fee: " & FormatCurrency(Session("StateFee"))
+                lblTotal.Text = "Total: " & FormatCurrency(Session("Total"))
+                lblTrades.Text = "Trades Fee: " & FormatCurrency(Session("Trades"))
+
+            End If
+
+
+        Catch ex As Exception
+            lblMessage.Text &= "A system error has occurred. Please contact the helpdesk:<br> " + ex.Message
+        End Try
+
+
+    End Sub
+
+
+    Protected Sub txtReset_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtReset.Click
+        Clear()
+    End Sub
+
+    Public Sub Clear()
+        ddlPermitType.SelectedIndex = 0
+        txtValue.Text = ""
+        Commercial.Checked = True
+        Residential.Checked = False
+        Session("PlansFee") = ""
+        Session("PermitFee") = ""
+        Session("Surcharge") = ""
+        Session("PermitValue") = 0
+        lblMessage.Text = ""
+        TradeNumbers = 0
+        ckbBuilding.Checked = False
+        ckbElectric.Checked = False
+        ckbRoof.Checked = False
+        ckbMechanic.Checked = False
+        ckbPlumbing.Checked = False
+        ckbGas.Checked = False
+        lblPermitFee.Text = ""
+        lblTrades.Text = ""
+        lblPlansFee.Text = ""
+        lblStateFee.Text = ""
+        lblTotal.Text = ""
+        lblEstimate.Text = ""
+        pnlResults.Visible = False
+    End Sub
+End Class
